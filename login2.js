ff7f82dd8c6dbd5752680e8f52477fd47be22b45
julia01herschmann <91904945+julia01herschmann@users.noreply.github.com>
2021-11-06 11:42:53 +0100

    Add files via upload

--- login2.js
+++ login2.js
@@ -1,16 +1,14 @@
-function validate()
-{
-    var username=document.getElementById("username").Value;
-    var password=document.getElementById("password").Value;
-    if(username=="admin"&& password=="user")
-{
-    alert("login succesfully");
-    return false;
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username, password);
+    if (username == "admin" && password == "user") {
+        alert("login succesfully");
+        return false;
 
-}
-else
-{
-    alert("login failed");
-}
+    }
+    else {
+        alert("login failed");
+    }
 
 }
